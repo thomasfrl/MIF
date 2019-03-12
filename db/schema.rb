0e79d0ce7945e674e5cb0696d4d1d5cabf4ffc23
Ljulienng <ljuliennguyen@gmail.com>
2019-03-12 16:25:19 +0100

    navbar stage 1 sync with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_03_11_215524) do
-=======
 ActiveRecord::Schema.define(version: 2019_03_12_114633) do
->>>>>>> 2f898307035ef2727f7e407f322373877ac09464
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
