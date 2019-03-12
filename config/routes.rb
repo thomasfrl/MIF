265d2a8e53be9beb4292bc7fce01bd9017612218
Arbousied <kiger.maxence@gmail.com>
2019-03-12 17:45:22 +0100

    first homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   resources :user_languages
   resources :tickets
   resources :correspondances
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   resources :flats
   resources :users
   resources :cities
+  resources :home
 
- 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
