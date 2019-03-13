7d78ea1ef478f632a6c7804682882e483d14ca26
Jeantirard <jean@tirard.net>
2019-03-13 10:47:10 +0100

    pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_03_12_152504) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -163,5 +160,4 @@ ActiveRecord::Schema.define(version: 2019_03_12_152504) do
   add_foreign_key "user_languages", "languages"
   add_foreign_key "user_languages", "users"
   add_foreign_key "users", "cities"
-
 end
