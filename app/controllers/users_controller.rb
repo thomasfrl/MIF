f36790e4027a0e0ee327919772bf7c1d9fdac032
Ljulienng <ljuliennguyen@gmail.com>
2019-03-13 17:28:21 +0100

    random stuffs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    @correspondances = User.find(params[:id]).primary_correspondances + User.find(params[:id]).secondary_correspondances
   end
 
   # GET /users/new
@@ -69,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :description, :age, :welcome_message, :city_id  , :nationality)
+      params.require(:user).permit(:first_name, :last_name, :description, :age, :welcome_message, :city_id, :nationality)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :city
+  belongs_to :city, optional: true
   has_one :flat
 
   has_many :tickets
