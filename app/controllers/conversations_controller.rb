1d3a2542e4592c75786bb57910eb88183a78cc4b
thomasfrl <feral.thomas@gmail.com>
2019-03-13 23:28:54 +0100

    scroll conv and scroll bottom messages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,56 +1,26 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_conversation, only: [:show, :edit, :update, :destroy]
+  before_action :set_conversation, only: [:destroy]
 
   # GET /conversations
-  # GET /conversations.json
   def index
     @conversations = Conversation.all.select{|c| c.participants.include?(current_user)}
   end
 
-  # GET /conversations/1
-  # GET /conversations/1.json
-  def show
-  end
-
-  # GET /conversations/new
-  def new
-    @conversation = Conversation.new
-  end
-
-  # GET /conversations/1/edit
-  def edit
-  end
-
-  # POST /conversations
-  # POST /conversations.json
   def create
     @conversation = Conversation.new(conversation_params)
 
     respond_to do |format|
       if @conversation.save
-        format.html { redirect_to @conversation, notice: 'Conversation was successfully created.' }
-        format.json { render :show, status: :created, location: @conversation }
+        format.html { redirect_to root_path }
+        format.js {}
       else
-        format.html { render :new }
-        format.json { render json: @conversation.errors, status: :unprocessable_entity }
+        format.html { render root_path}
+        format.js {}
       end
     end
   end
 
-  # PATCH/PUT /conversations/1
-  # PATCH/PUT /conversations/1.json
-  def update
-    respond_to do |format|
-      if @conversation.update(conversation_params)
-        format.html { redirect_to @conversation, notice: 'Conversation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @conversation }
-      else
-        format.html { render :edit }
-        format.json { render json: @conversation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /conversations/1
   # DELETE /conversations/1.json
