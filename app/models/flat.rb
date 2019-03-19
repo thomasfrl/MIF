848a330f7001771f4cce3dd61ed5c75254213d26
thomasfrl <feral.thomas@gmail.com>
2019-03-19 21:21:57 +0100

    flat front edit

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,7 +1,13 @@
 class Flat < ApplicationRecord
+  before_validation :default_values
   belongs_to :user
   has_many_attached :pictures
-  validates :room, inclusion: { in: ["Bed-room", "Living-room"], message: "%{value} is not a valid room"}
-  validates :sleep, inclusion: { in: ["Couch", "Bed"], message: "%{value} is not a valid sleep"}
+  validates :room, inclusion: { in: ["Bed-room", "Living-room", ""], message: "%{value} is not a valid room"}
+  validates :sleep, inclusion: { in: ["Couch", "Bed",""], message: "%{value} is not a valid sleep"}
   validates :place, numericality: { greater_than: 0, less_than: 4 }
+  def default_values
+    self.room ||= ''
+    self.sleep ||= ''
+    self.place ||= 1
+  end 
 end
