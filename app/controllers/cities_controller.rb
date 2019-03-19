f12863d41c930ab7609bae9f20071e1391bd68d7
greg-Da <gregoiredalmasso@gmail.com>
2019-03-19 17:06:43 +0100

    nav update + cities index fixed

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,7 +4,7 @@ class CitiesController < ApplicationController
   # GET /cities
   # GET /cities.json
   def index
-    @cities = City.all
+    @cities = City.all.slice(0...8)
   end
 
   # GET /cities/1
