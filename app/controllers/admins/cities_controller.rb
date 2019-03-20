f5706064c8cdb8b6832e424e431fa0b16501db18
thomasfrl <feral.thomas@gmail.com>
2019-03-20 15:49:30 +0100

    first try cities admin

--- app/controllers/admins/cities_controller.rb
+++ app/controllers/admins/cities_controller.rb
@@ -1,66 +1,37 @@
 class Admins::CitiesController < ApplicationController
-  before_action :set_city, only: [:show, :edit, :update, :destroy]
+  before_action :set_city, only: [:update, :destroy]
 
   # GET /cities
-  # GET /cities.json
   def index
     @cities = City.all
-  end
-
-  # GET /cities/1
-  # GET /cities/1.json
-  def show
-    @city = City.find(params[:id])
-    @cities = City.all
-  end
-
-  # GET /cities/new
-  def new
-    @city = City.new
-  end
-
-  # GET /cities/1/edit
-  def edit
+    @city_new = City.new
   end
 
   # POST /cities
-  # POST /cities.json
   def create
     @city = City.new(city_params)
-
-    respond_to do |format|
-      if @city.save
-        format.html { redirect_to @city, notice: 'City was successfully created.' }
-        format.json { render :show, status: :created, location: @city }
-      else
-        format.html { render :new }
-        format.json { render json: @city.errors, status: :unprocessable_entity }
-      end
+    if @city.save
+      redirect_to admins_cities_path, notice: 'City was successfully created.' 
+    else
+      flash[:danger] =  'City was not successfully created.'
+      redirect_to admins_cities_path 
     end
   end
 
   # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
-    respond_to do |format|
-      if @city.update(city_params)
-        format.html { redirect_to @city, notice: 'City was successfully updated.' }
-        format.json { render :show, status: :ok, location: @city }
-      else
-        format.html { render :edit }
-        format.json { render json: @city.errors, status: :unprocessable_entity }
-      end
+    if @city.update(city_params)
+      redirect_to admins_cities_path, notice: 'City was successfully updated.' 
+    else
+      flash[:danger] =  'City was not successfully updated.'
+      redirect_to admins_cities_path 
     end
   end
 
   # DELETE /cities/1
-  # DELETE /cities/1.json
   def destroy
     @city.destroy
-    respond_to do |format|
-      format.html { redirect_to cities_url, notice: 'City was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to cities_url, notice: 'City was successfully destroyed.'
   end
 
   private
