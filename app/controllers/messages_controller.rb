10905df49325af505a61ec2043f161e45f01697c
thomasfrl <feral.thomas@gmail.com>
2019-03-21 16:58:35 +0100

    manque la localisation de la ville

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,9 @@ class MessagesController < ApplicationController
   def index
     options = { units: "metric", APPID: "1111111111" }
     ow = Rails.configuration.open_weather_api
-    @weather = ow.current city: 'Santa Cruz de Tenerife', country_code: 'es'
+    @weather = ow.current city: 'Paris', country_code: 'fr'
+    @icon = @weather['weather'][0]['icon']
+    @temp = (@weather['main']['temp'].to_f).to_f - 273.1
     @conversation = Conversation.find(params[:conversation_id])
     @messages = Message.order(:created_at).where(conversation: @conversation)
     @other_user = @conversation.other_participant(current_user)
--- config/initializers/open-weather-api.rb
+++ config/initializers/open-weather-api.rb
@@ -6,7 +6,7 @@ OpenWeatherAPI.configure do |config|
   config.api_key = ENV['WEATHER_KEY']
 
   # Optionals
-  config.default_language = 'es'     # 'en' by default
-  config.default_country_code = 'es' # nil by default (ISO 3166-1 alfa2)
+  config.default_language = 'en'     # 'en' by default
+  config.default_country_code = 'en' # nil by default (ISO 3166-1 alfa2)
   config.default_units = 'metric'    # 'metric' by default
 end
