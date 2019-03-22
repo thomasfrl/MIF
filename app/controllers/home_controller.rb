8816da479a416a01c91277b97025b7e76fbdd6fc
Ljulienng <ljuliennguyen@gmail.com>
2019-03-22 18:01:39 +0100

    homeview random matched users almost done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @cities = City.all.slice(0...8)
     @random_cities = City.all.sample
-    
+    random_user
 
   end
 
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
 
   def contact
   end
-  
+
   def team
   end
 
@@ -24,12 +24,27 @@ class HomeController < ApplicationController
   private
 
   def random_user
-  users = []
-    10.times do
-      users << User.all.sample
-    end
-  @users = users
 
+    @users = User.all
+    rand_users = []
+    @user_matched = []
+    @users.each do |user|
+      if user != current_user
+        if user.matchmaking(current_user) > 2
+          rand_users << user
+        end
+      end
+    end
+
+    if rand_users.size < 8
+      @user_matched += rand_users
+    else
+      8.times do
+        @user_matched << rand_users.sample
+      end
+    end
+
+    return @user_matched
   end
 end
 
